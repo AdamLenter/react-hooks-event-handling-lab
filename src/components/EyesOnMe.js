96c260252ba64a5d346feba898fc92e54cffc4ac
Adam <adamlenter1@gmail.com>
2021-12-08 14:31:13 -0500

    commit tree 658606ca0dfa2b4e7610fdbdabe170baa542097c

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,10 @@
+import React from 'react';
+
 // Code EyesOnMe Component Here
+function EyesOnMe(props) {
+    return (
+        <button id = "eyesOnMe" onFocus = {()=>console.log(`Good!`)} onBlur = {()=>console.log('Hey! Eyes on me!')}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
-// Code Keypad Component Here
+import React, { useState } from 'react';
+
+function Keypad(props) {
+    return (
+        <form>
+            <label>Password:</label>
+            <input type = "password" id = "password" value = "" onChange = {()=>console.log("Entering password...")}/>
+        </form>
+    )
+}
+
+export default Keypad;
